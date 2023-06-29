fe6ba31543f2a87f63d8591d880393d7ec57f60b
camilousa <83974882+camilousa@users.noreply.github.com>
2023-06-29 12:33:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="Linear regression diagnostics",
+    name="linear_reg_diagnostic",
     version="1.0",
     packages=find_packages(),
     url="https://www.statsmodels.org/dev/examples/notebooks/generated/linear_regression_diagnostics_plots.html"
